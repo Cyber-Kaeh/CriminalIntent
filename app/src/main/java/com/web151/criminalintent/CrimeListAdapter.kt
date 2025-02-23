1bd5bc2945a44b1f60091d03fa1f5d3e994db341
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-02-23 10:30:47 -0500

    Finished Ch. 10

--- app/src/main/java/com/web151/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/web151/criminalintent/CrimeListAdapter.kt
@@ -2,13 +2,25 @@ package com.web151.criminalintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.web151.criminalintent.databinding.ListItemCrimeBinding
 
 class CrimeHolder(
-    val binding: ListItemCrimeBinding
+    private val binding: ListItemCrimeBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
 
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }
 
 class CrimeListAdapter(
@@ -23,10 +35,7 @@ class CrimeListAdapter(
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-        }
+        holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
